c292d48f72bdf91c46d7bae18181fa8f1d592049
DavidGoedicke <2676840+DavidGoedicke@users.noreply.github.com>
2018-10-16 16:46:01 -0400

    fixed billboard

--- Assets/_Workspaces/far-lab/Reviews/Billboard.cs
+++ Assets/_Workspaces/far-lab/Reviews/Billboard.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour {
+    Transform player;
 
+    void Start(){
+        player = FindObjectOfType<VehicleController>().transform;
+    }
 	// Update is called once per frame
 	void Update() 
 	{
-		transform.LookAt(Camera.main.transform.position, Vector3.up);
+       // Debug.DrawLine(transform.position, player.position);
+       // Debug.Log(player.position);
+		transform.LookAt(player, Vector3.up);
 	}
-}
+} 
