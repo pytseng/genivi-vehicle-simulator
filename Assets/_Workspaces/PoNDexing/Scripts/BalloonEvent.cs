efe9afd3a8be6a1c9c0a3b816d69fc9d53a11f73
pytseng <tw.pytseng@gmail.com>
2018-12-02 10:59:03 -0500

    minor

--- Assets/_Workspaces/PoNDexing/Scripts/BalloonEvent.cs
+++ Assets/_Workspaces/PoNDexing/Scripts/BalloonEvent.cs
@@ -9,18 +9,19 @@ public class BalloonEvent : MonoBehaviour {
     void Start () {
         player_t = _GetTargetObeject().transform;
         balloon = Resources.Load("prefabs/balloon") as GameObject;
+        InvokeRepeating("BalloonFest", 2.0f, 10f);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //InvokeRepeating("BalloonFest", 2.0f, 1f);
+
     }
     private GameObject _GetTargetObeject()
     {
         return FindObjectOfType<VehicleController>().gameObject;
     }
     private void BalloonFest(){
-        var spawningSphere = new Vector3(Random.Range(-10f,10f), Random.Range(0f, 10f), Random.Range(0f, 10f));
-        Instantiate(balloon, player_t.position, Quaternion.identity);
+        var spawningSphere = new Vector3(Random.Range(10f,30f), Random.Range(0f, 5f), Random.Range(-5f, 5f));
+        Instantiate(balloon, player_t.position + spawningSphere, Quaternion.identity);
     }
 }
