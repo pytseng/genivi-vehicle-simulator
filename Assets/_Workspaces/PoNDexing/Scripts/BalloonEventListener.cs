9cf012779dd97284a87d728a70fdbc96389eebaa
David Goedicke <da.goedicke@gmail.com>
2018-12-02 15:59:19 -0500

    update balloon controls

---
+++ Assets/_Workspaces/PoNDexing/Scripts/BalloonEventListener.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonEventListener : MonoBehaviour {
+	bool eventTriggered;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		eventTriggered = false;
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!eventTriggered) {
+			if (Input.GetKeyDown(KeyCode.B)) {
+				player = FindObjectOfType<VehicleController>().gameObject;
+				if (!eventTriggered) {
+					if (player.GetComponent<BalloonEvent>() == null) {
+						player.AddComponent<BalloonEvent>();
+						eventTriggered = true;
+					}
+				}
+			}
+		} else {
+			if (Input.GetKeyDown(KeyCode.B)) {
+				player = FindObjectOfType<VehicleController>().gameObject;
+				if (player.GetComponent<BalloonEvent>()) {
+					Destroy(player.GetComponent<BalloonEvent>());
+					eventTriggered = false;
+				}
+			}
+		}
+
+	}
+}
